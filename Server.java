00cf9c1232f0dd055c80599440f1a6f75074e867
StVitalii <stvitaliy2001@gmail.com>
2021-12-02 16:09:15 +0200

    initial

---
+++ Server.java
@@ -0,0 +1,42 @@
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.IOException;
+
+public class Server {
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer() {
+        try {
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                System.out.println("A new client has connected");
+                ClientHandler ClientHandler = new ClientHandler(socket);
+
+                Thread thread = new Thread(ClientHandler);
+                thread.start();
+            }
+        } catch (IOException e) {
+            closeServerSocket();
+        }
+    }
+
+    public void closeServerSocket() {
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String args[]) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(6666);
+        Server server = new Server(serverSocket);
+        server.startServer();
+    }
+}
